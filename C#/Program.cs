bbb0fabefcbe4cc6b7fa3b08044c78b866f19fef
Kir444ik <144593324+Kir444ik@users.noreply.github.com>
2023-12-06 12:12:53 +0300

    Code C#

---
+++ C#/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Enter quantity word");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = CreatArray(size);
+PrintArray(array);
+PrintM(array);
+string[] CreatArray ( int size ){
+    string[] arr = new string[size];
+
+    for ( int i = 0 ; i < size ; i++){
+        Console.WriteLine("Enter word");
+        arr[i] = Convert.ToString(Console.ReadLine());
+    }
+    return arr;
+
+}
+void PrintArray(string[] arr){
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++){
+        if (i < arr.Length - 1 ) Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]--->");
+}
+void PrintM(string[] arr){
+     Console.Write("[");
+    for (int i = 0; i < arr.Length; i++){
+        if (arr[i].Length < 4 ) Console.Write($"{arr[i]},");
+    }
+    Console.WriteLine("]--->");    
+}
